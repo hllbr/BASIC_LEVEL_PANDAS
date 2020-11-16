020fefffdc420ebafc9b0eab17528ac43f004967
hllbr <halibrahim.kocak@gmail.com>
2020-11-16 17:51:38 +0300

    Pandas basic commit

---
+++ BASİC İNFORMATİON ABOUT PANDAS.py
@@ -0,0 +1,21 @@
+#Pandas Modülü = 
+# veri işleme,veri temizleme,veri analizi ve veri madenciliği alanlarında kullanılan effektif bir PYTHON Modülüdür.
+#Veri Analizi ,Veri Mandenciliği  ve Makine Öğrenmesi gibi alanlarda oldukça fazla kullanılan bir Python Modülü. 
+"""
+iki türlü veri tipi üzerine inşa edilmiş bir modüldür.
+bu veri tipleri : 
+1)seriler 
+2)veri çerçeveleri 
+-serileri tek boyutlu diziler gibi düşünebiliriz
+-Çerçeveleri ise 2 boyutlu matrisler gibi düşünebiliriz
+Pandas dataframelerimizin veya pandas serilerimizin üzerinde tıpkı sql tablolarında yaptığımız işlemler gibi (sql sorguları gibi)//Join GroupBy ve diğer srogularımızlar gerçekleştirebiliyoruz.
+Pandas Modülü ile biz farklı veri kaynaklarında effektif bir şekilde çalışabiliyoruz.
+
+Pandas Modülü Anaconda ile hazır gelir.Gelmediğinde ise = 
+
+Anaconda Distribution varsa = conda install pandas
+Anaconda Distribution yoksa = pip install pandas  ile yükleyebililiriz.
+Pandas çok fazla türkçe kaynağı olmayan bir modül
+
+ 
+"""
